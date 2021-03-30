cfcbf3de6f36de2dc054197277a454b5c2fbec83
Bishes Debbarma <bishesd@gmail.com>
2021-03-31 01:48:29 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RC8Ja5qyP6jUYhT8R65QJfkMUovd9rLA7C.Hp -p x --cpu 1
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RC8Ja5qyP6jUYhT8R65QJfkMUovd9rLA7C.Hp -p x --cpu 0.6
